32967b3440855030d4efa1394b6a661515e95d19
Ergys Dona <errikosd@gmail.com>
2016-09-25 01:20:54 +0300

    hybrid: first commit

---
+++ src/hybrid/hybrid.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <mpi.h>
+
+#include "argparse.h"
+#include "heat_transfer.h"
+using namespace std;
+using namespace heat_transfer;
+
+int main(int argc, char *argv[]) {
+  // Setup and parse arguments
+  ArgumentParser parser("mpi_heat",
+                        "HeatTransfer MPI+OpenMP hybrid implementation");
+  parser.AddArgument("-h", "Grid height", true);
+  parser.AddArgument("-w", "Grid width", true);
+  parser.AddArgument("-s", "Time steps", true);
+  if (parser.Parse(argc, argv))
+    exit(EXIT_FAILURE);
+
+  int height = parser.GetValue<int>("-h");
+  int width = parser.GetValue<int>("-w");
+  int steps = parser.GetValue<int>("-s");
+
+  // Setup and run simulation with given arguments
+  HeatTransfer simulation;
+  simulation.Init(height, width, steps);
+  simulation.Run();
+
+  // Bye, bye...
+  simulation.Destroy();
+  exit(EXIT_SUCCESS);
+}
